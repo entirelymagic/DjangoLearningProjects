2b54003187cef2f64c948c9f59e6ff99c31d38c2
entirelymagic@gmail.com <@lexandr@123>
2021-05-07 14:46:52 +0300

    Added Products from category to product_lists

--- MyShop/shop/urls.py
+++ MyShop/shop/urls.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 from . import views
 
+
+app_name = 'shop'
+
 urlpatterns = [
-    path('categories', views.product_list),
-    path('categories/<slug:category_slug>', views.product_list),
-    path('', views.CategoryTemplate.as_view()),
+    path('', views.product_list, name='product_list'),
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.product_details, name='product_detail'),
 ]
--- MyShop/shop/views.py
+++ MyShop/shop/views.py
@@ -12,8 +12,8 @@ class CategoryListView(ListView):
     queryset = Category.objects.all()
 
 
-class CategoryTemplate(TemplateView):
-    template_name = "category.html"
+# class CategoryTemplate(TemplateView):
+#     template_name = "category.html"
 
 
 def product_list(request, category_slug=None):
@@ -23,10 +23,13 @@ def product_list(request, category_slug=None):
     if category_slug:
         category_name = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=category_name)
-    return render(request, 'category.html', {
+    return render(request, 'product/product_list.html', {
         'products': products,
         'category': category_name,
         'categories': categories,
     })
 
 
+def product_details(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, 'shop/product/detail.html', {'product': product})
